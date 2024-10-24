2853ac04ee34ba0e07b41a6454d0b76aea4fdec1
Felipe <labsfiap@fiap.com.br>
2024-10-24 11:00:25 -0300

    Criando o script do torneio

---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+function inserir(){
+    let numero = document.getElementById('posicao').value -1;
+    let novo = document.getElementById('novo').value;
+    document.getElementByClassName("time").immerHTML= novo;
+}
